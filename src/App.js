a7d33c01c7f09550b1ee1d65ab1b46b690f59865
Tom <thomas.a.butler@gmail.com>
2018-01-26 11:54:46 -0700

    refactor to es6 class component & ref a DOM element

--- src/App.js
+++ src/App.js
@@ -158,12 +158,52 @@ class App extends Component {
   }
 }
 
+/* functional stateless component, before
+* refactoring to ES6 class component
 const Search = ({ value, onChange, onSubmit, children }) => (
   <form onSubmit={onSubmit}>
-    <input type="text" value={value} onChange={onChange} />
+    <input 
+      type="text" 
+      value={value} 
+      onChange={onChange}
+      ref={(node) => {this.intput = node;}} />
     <button type="submit"> {children} </button>
   </form>
 );
+*/
+
+class Search extends Component {
+  /**
+   * this object of ES6 class component allows referencing 
+   * DOM node with ref attribute
+   * focus on input field when component is mounted
+   * 
+   * For stateless component (no this), use
+   * let input;
+   * <input... ref={(node) => input = node }
+   */
+  componentDidMount(){
+    if (this.input){
+      this.input.focus();
+    }
+  }
+  render() {
+    const {
+      value, onChange, onSubmit, children
+    } = this.props;
+
+    return (
+    <form onSubmit={onSubmit}>
+      <input 
+        type="text"
+         value={value} 
+         onChange={onChange} 
+         ref={(node) => {this.input = node;}}/>
+      <button type="submit"> {children} </button>
+    </form>
+    )
+  }
+}
 
 const Table = ({ list, onDismiss }) => (
   <div className="table">
